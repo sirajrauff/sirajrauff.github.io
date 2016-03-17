3343c3e2117e7bee2f63137b4872d2bb3ed168ac
Siraj Rauff <sirajrauff@gmail.com>
2016-03-17 00:57:27 -0400

    Reduce animation delay

--- dist/js/index.js
+++ dist/js/index.js
@@ -2,7 +2,7 @@ $.backstretch([
   'images/background_image0.jpg',
   'images/background_image1.jpg',
   'images/background_image2.jpg'
-], {duration: 3000, fade: 1500});
+], {duration: 1500, fade: 1500});
 
 $(".0").fadeIn(1500);
 
