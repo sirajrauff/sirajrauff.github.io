dc9a5a43e88daf54848f8531c5846acff1522d59
Siraj Rauff <sirajrauff@gmail.com>
2016-03-17 00:55:12 -0400

    Rework slideshow
    - Use Backstretch instead of Bootstrap Carousel
    - Fades are now more elegant

---
+++ dist/js/index.js
@@ -0,0 +1,12 @@
+$.backstretch([
+  'images/background_image0.jpg',
+  'images/background_image1.jpg',
+  'images/background_image2.jpg'
+], {duration: 3000, fade: 1500});
+
+$(".0").fadeIn(1500);
+
+$(window).on("backstretch.before", function (e, instance, index) {
+  $(".overlay").not("." + index).fadeOut(1500);
+  $("." + index).fadeIn(1500);
+});
